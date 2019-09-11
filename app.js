6a25784ce216852783cb4521ca83c96790fc9192
Julieta <juliiy98@gmail.com>
2019-09-11 09:36:12 -0600

    Servidor síncrono que lea y envíe un archivo.

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const http = require("http");
+const fs = require("fs");
 
- function init(){
-     function onRequest(request, response){
-        response.writeHead(200, {"Content-Type":"text/html"});
-        response.write("<h1>Hola Mundo</h1>")
+exports.init = () => {
+    const html = fs.readFileSync("./index.html");
+    http.createServer((request, response) => {
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.write(html)
         response.end();
-     }
-
-     http.createServer(onRequest).listen(1111);
- }
-init();
-//exports.init = init;
+    }).listen(1111);
+};

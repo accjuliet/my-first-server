42819c767f0984762de1c5f486ae8d7dc037b0b7
Julieta <juliiy98@gmail.com>
2019-09-11 09:50:01 -0600

    Servidor asincrono

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const http = require("http");
 const fs = require("fs");
 
 exports.init = () => {
-    const html = fs.readFileSync("./index.html");
     http.createServer((request, response) => {
-        response.writeHead(200, { "Content-Type": "text/html" });
-        response.write(html)
-        response.end();
+        fs.readFile("./index.html", (err, obj) => {
+            if (err) {
+                response.writeHead(404, { "Content-Type": "text/html" });
+            } else {
+                response.writeHead(200, { "Content-Type": "text/html" });
+                response.write(obj)
+            }
+            response.end();
+        });
+
     }).listen(1111);
 };
